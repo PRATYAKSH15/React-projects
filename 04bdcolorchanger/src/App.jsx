342d9fbd0059cc4e88a46e86e240fd371a96de84
unknown <pratyaksh1594@gmail.com>
2025-01-23 22:44:07 +0530

    added purple color in 04project in app.jsx

--- 04bdcolorchanger/src/App.jsx
+++ 04bdcolorchanger/src/App.jsx
@@ -58,6 +58,15 @@ function App() {
             Orange
           </button>
 
+          <button
+            onClick={() => setColor("purple")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "purple" }}
+          >
+            Purple
+          </button>
+
+
         </div>
       </div>
     </div>
